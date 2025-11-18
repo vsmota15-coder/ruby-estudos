3bc9b3185a96e918db03e105e11f1d4e945fd413
vsmota15-coder <vsmota15@gmail.com>
2025-11-18 11:40:40 -0300

    calculo de idade

---
+++ dia1.rb
@@ -0,0 +1,12 @@
+#puts "Ola mundo"
+#tempo_restante = "6 semanas"
+#puts "tenho #{tempo_restante} para fazer isso!"
+
+
+nome = "Vitor "
+ano_nascimento = "2005"
+ano_atual = "2025"
+
+idade_calculada = ano_atual.to_i - ano_nascimento.to_i
+
+puts "#{nome} nasceu em #{ano_nascimento} e tem #{idade_calculada} anos "
