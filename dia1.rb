8f53fb82f67771ab5adfb9d0d312c3821e5fe5a1
vsmota15-coder <vsmota15@gmail.com>
2025-11-18 17:31:55 -0300

    Atualização do calculo de idade

--- dia1.rb
+++ dia1.rb
@@ -3,10 +3,28 @@
 #puts "tenho #{tempo_restante} para fazer isso!"
 
 
-nome = "Vitor "
-ano_nascimento = "2005"
-ano_atual = "2025"
+#nome = "Vitor"
+#ano_nascimento = "2005"
+#ano_atual = "2025"
 
-idade_calculada = ano_atual.to_i - ano_nascimento.to_i
+#idade_calculada = ano_atual.to_i - ano_nascimento.to_i
 
-puts "#{nome} nasceu em #{ano_nascimento} e tem #{idade_calculada} anos "
+#puts "#{nome} nasceu em #{ano_nascimento} e tem #{idade_calculada} anos "
+
+puts "Qual é o seu nome ?"
+nome = gets.chomp 
+
+puts "Qual é o seu ano de nascimento?"
+ano_nascimento = gets.chomp.to_i
+
+ano_atual = 2025
+
+idade_calculada = ano_atual - ano_nascimento
+
+puts "#{nome} nasceu em #{ano_nascimento} e tem #{idade_calculada} anos"
+
+if idade_calculada >= 18
+  puts "Você é maior de idade"
+else
+  puts "Voce é menor de idade"
+end
